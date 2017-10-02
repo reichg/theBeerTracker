7684514e593abeb606c2dc440f428a77f700b463
reichg <gabe.reichenberger@gmail.com>
2017-10-02 04:06:41 -0700

    Pages made, need to store in database

--- src/main/java/gabe/beertracker/theBeerTracker/controllers/UserController.java
+++ src/main/java/gabe/beertracker/theBeerTracker/controllers/UserController.java
@@ -1,24 +1,31 @@
-package gabe.beertracker.theBeerTracker.Controllers;
+package gabe.beertracker.theBeerTracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+
 @Controller
-public class LoginController {
+public class UserController {
 
     @RequestMapping(value = "register")
     public String register() {
+
         return "register";
     }
 
     @RequestMapping(value = "login")
     public String login() {
+
         return "login";
     }
 
     @RequestMapping(value = "home")
-    public String home() {
+    public String home(Model model) {
+
+
+        model.addAttribute("title", "Hello User" );
         return "home";
     }
 
