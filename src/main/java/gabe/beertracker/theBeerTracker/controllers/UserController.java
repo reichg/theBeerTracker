b2a16eaae56d834aeebc1f7e9420866561e3e0e6
reichg <gabe.reichenberger@gmail.com>
2017-10-02 16:38:08 -0700

    trying to pass userName to database.

--- src/main/java/gabe/beertracker/theBeerTracker/controllers/UserController.java
+++ src/main/java/gabe/beertracker/theBeerTracker/controllers/UserController.java
@@ -1,24 +1,52 @@
 package gabe.beertracker.theBeerTracker.controllers;
 
+import gabe.beertracker.theBeerTracker.models.User;
+import gabe.beertracker.theBeerTracker.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserDao userDao;
+
+    @RequestMapping(value = "")
+    public String index() {
+
+        return "index";
+    }
+
     @RequestMapping(value = "register")
-    public String register() {
+    public String Register() {
 
         return "register";
     }
 
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String provessRegister(@ModelAttribute @Valid User newUser, Model model) {
+
+        userDao.save(newUser);
+        return "index";
+    }
+
     @RequestMapping(value = "login")
-    public String login() {
+    public String displayLogin() {
 
         return "login";
+    }
+
+    @RequestMapping(value = "login")
+    public String processLogin() {
+
+        return "index";
     }
 
     @RequestMapping(value = "home")
