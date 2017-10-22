cab35807d3fa25493cdc7252e404fa85c4899c8d
Robert1GA <roberts.2015@yahoo.co.uk>
2017-10-21 23:40:29 -0700

    Hacked a locations page route. Starting building out locations page. Changed breadcrumbs into a fragment and refactored into wheresmybeer and locations pages.

--- src/main/java/gabe/beertracker/theBeerTracker/controllers/UserController.java
+++ src/main/java/gabe/beertracker/theBeerTracker/controllers/UserController.java
@@ -133,6 +133,28 @@ public class UserController {
         return "wheresmybeer";
 
     }
+
+    // 1
+    @RequestMapping(value = "locations")
+    public String locations(HttpServletRequest request, Model model, Beer beer){
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "redirect:/login";
+        }
+        User storedData = (User)session.getAttribute("loggedInUser"); //should retrieve the stored session?
+        User user = userDao.findOne(storedData.getId());
+
+        Iterable<Beer> beerList = beerDao.getBeersTriedByUserId(storedData.getId());
+
+        model.addAttribute("beer", beer);
+        model.addAttribute("beerList", beerList);
+        model.addAttribute("userName", storedData.getUserName());
+//        model.addAttribute("welcome", "Welcome, " + user.getUserName());
+
+        return "locations";
+
+    }
+
 }
 
 
