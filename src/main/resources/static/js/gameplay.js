773d857ea43dddbb266f852cf7e056dc035656a9
Robert1GA <roberts.2015@yahoo.co.uk>
2017-10-24 22:17:35 -0700

    image files

--- src/main/resources/static/js/gameplay.js
+++ src/main/resources/static/js/gameplay.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     console.log( "page ready!" );
+
+    var bottleImages = [
+        "amber-beer-bottle-foil-neck.png",
+        "amber-beer-bottle-square-etiquette.png",
+        "amber-beer-steinie-bottle.png",
+        "brown-beer-bottle.png",
+        "\img\\clear-beer-bottle.png",
+        "dark-beer-bottle.png",
+        "green-beer-bottle-etiquette.png",
+        "green-beer-bottle-neck-foil.png",
+        "green-beer-bottle.png",
+        "lt-brown-beer-bottle.png"
+    ];
+
+    document.getElementById('bottle-image').src = bottleImages[4];
 });
