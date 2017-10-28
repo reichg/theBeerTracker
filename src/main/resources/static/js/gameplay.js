f585aee536586c4c0074a049d5d3eb8873ddd632
Robert1GA <roberts.2015@yahoo.co.uk>
2017-10-28 10:50:53 -0700

    removed found button

--- src/main/resources/static/js/gameplay.js
+++ src/main/resources/static/js/gameplay.js
@@ -6,15 +6,15 @@ $( document ).ready(function() {
         "amber-beer-bottle-square-etiquette.png",
         "amber-beer-steinie-bottle.png",
         "brown-beer-bottle.png",
-        "clear-beer-bottle.png",
-        "dark-beer-bottle.png",
+        //"clear-beer-bottle.png",
+        //"dark-beer-bottle.png",
         "green-beer-bottle-etiquette.png",
         "green-beer-bottle-neck-foil.png",
         "green-beer-bottle.png",
         "lt-brown-beer-bottle.png"
     ];
 
-    var rand = Math.floor(Math.random() * 10);
+    var rand = Math.floor(Math.random() * bottleImages.length);
     console.log(rand);
     document.getElementById('bottle-image').src = '\img\\' + bottleImages[rand];
 });
